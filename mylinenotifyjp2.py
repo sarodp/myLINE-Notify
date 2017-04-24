5d2412d3a30b397264ce0dbb77cbf19aeb3f9af3
sarodp <sarodp@yahoo.com>
2017-04-24 11:18:30 +0700

    Add files via upload

---
+++ mylinenotifyjp2.py
@@ -0,0 +1,58 @@
+#!usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import urllib.parse
+import pycurl,time 
+
+#--https://notify-bot.line.me/my/ 
+#--you must generate token_key here first.
+token_key = '<your token_key>'
+headers = str("Authorization: Bearer "+token_key)
+
+#--LINE Notify Image File
+def line_notifyIMG(xmessage,imagefile,xheaders):
+	curl = pycurl.Curl()
+	curl.setopt(pycurl.URL, 'https://notify-api.line.me/api/notify')
+	curl.setopt(pycurl.HTTPHEADER, [headers])
+
+	curl.setopt(pycurl.HTTPPOST, [
+	('message', xmessage.encode('utf-8')),
+	('imageFile', (curl.FORM_FILE, str(imagefile))) ])
+
+	curl.perform()
+
+
+#--LINE Notify sticker
+def line_notifySTK(xmessage,stkpid,stkid,xheaders):
+	curl = pycurl.Curl()
+	curl.setopt(pycurl.URL, 'https://notify-api.line.me/api/notify')
+	curl.setopt(pycurl.HTTPHEADER, [xheaders])
+
+	curl.setopt(pycurl.HTTPPOST, [
+	('message', xmessage.encode('utf-8')),
+	('stickerPackageId', str(stkpid)),
+	('stickerId', str(stkid)) ])
+
+	curl.perform()
+
+#--LINE Notify Message
+def line_notifyMSG(xmessage,xheaders):
+	curl = pycurl.Curl()
+	curl.setopt(pycurl.URL,'https://notify-api.line.me/api/notify')
+	curl.setopt(pycurl.HTTPHEADER,[xheaders])
+	curl.setopt(pycurl.HTTPPOST,[('message', xmessage.encode('utf-8'))])
+	curl.perform()
+
+if __name__ == "__main__":
+	xmsg=("ทดสอบส่งสติกเกอร์  วัน-เวลา %s" % (time.ctime()))
+	stkpid = 1
+	stkid = 115
+	line_notifySTK(xmsg,stkpid,stkid,headers)
+
+	xmsg=("ทดสอบส่งข้อความภาษาไทย  วัน-เวลา %s" % (time.ctime()))
+	line_notifyMSG(xmsg,headers)
+
+	xfile ='myphoto2.png'
+	xmsg=("%s รูปภาพดญ.โปรแกรม  วัน-เวลา %s" % (xfile,time.ctime()))
+	line_notifyIMG(xmsg,xfile,headers)
+
